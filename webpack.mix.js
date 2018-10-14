419f7b8154ac26bdc39183be0ef7530b86843ddf
sile <sinisa.buncic7@gmail.com>
2018-10-14 21:39:43 +0200

    Updated gitignore and webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
+// Server side
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+// Client side
+mix.js('resources/client/scripts/main.js', 'public/client/js')
+  .sass('resources/client/assets/scss/style.scss', 'public/client/css');
 
 mix.browserSync('http://localhost:8000');
