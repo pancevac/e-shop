6929d9dd635524e96bc4e863db8466604229a476
sile <sinisa.buncic7@gmail.com>
2018-10-14 21:24:28 +0200

    Added client theme style files

---
+++ resources/client/scripts/main.js
@@ -0,0 +1,23 @@
+
+require('../../js/bootstrap');
+/*require('../assets/js/jquery-3.2.1.min');
+require('../assets/js/popper');
+require('../assets/js/bootstrap.min');*/
+require('../assets/js/stellar');
+require('../assets/vendors/lightbox/simpleLightbox.min');
+require('../assets/vendors/nice-select/js/jquery.nice-select');
+require('../assets/vendors/isotope/imagesloaded.pkgd.min');
+require('../assets/vendors/owl-carousel/owl.carousel.min');
+require('../assets/js/jquery.ajaxchimp.min');
+require('../assets/vendors/counter-up/jquery.waypoints.min');
+require('../assets/vendors/flipclock/timer');
+require('../assets/vendors/jquery-ui/jquery-ui');
+require('../assets/vendors/counter-up/jquery.counterup');
+require('../assets/js/mail-script');
+require('../assets/js/theme');
+
+window.Vue = require('vue');
+
+const app = new Vue({
+  el: '#app'
+});
