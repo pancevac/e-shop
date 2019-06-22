2e3e0d880bb5f274e6734a97e2e64515e36472d1
Sile <sinisa.buncic7@gmail.com>
2019-06-22 15:40:46 +0200

    Fixed bug with permission on admin panel.

--- resources/js/components/partials/LeftBar.vue
+++ resources/js/components/partials/LeftBar.vue
@@ -8,18 +8,18 @@
           <li class="sidebar-item"> <router-link :to="'/home'" class="sidebar-link waves-effect waves-dark sidebar-link" href="#" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Početna</span></router-link></li>
 
           <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-account-key"></i><span class="hide-menu">Podešavanja </span></a>
-            <ul aria-expanded="false" class="collapse  first-level">
-              <li class="sidebar-item"><router-link :to="'/menus'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Meni </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/banners'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Baneri </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/widgets'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Widžeti </span></router-link></li>
+            <ul v-if="user" aria-expanded="false" class="collapse  first-level">
+              <li v-if="can('menu.view')" class="sidebar-item"><router-link :to="'/menus'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Meni </span></router-link></li>
+              <li v-if="can('banner.view')" class="sidebar-item"><router-link :to="'/banners'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Baneri </span></router-link></li>
+              <li v-if="can('widget.view')" class="sidebar-item"><router-link :to="'/widgets'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Widžeti </span></router-link></li>
             </ul>
           </li>
 
           <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-account-key"></i><span class="hide-menu">Korisici </span></a>
-            <ul aria-expanded="false" class="collapse  first-level">
-              <li class="sidebar-item"><router-link :to="'/users'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Korisnici </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/permissions'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Dozvole </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/roles'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Uloge </span></router-link></li>
+            <ul v-if="user" aria-expanded="false" class="collapse  first-level">
+              <li v-if="can('user.view')" class="sidebar-item"><router-link :to="'/users'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Korisnici </span></router-link></li>
+              <li v-if="can('permission.view')" class="sidebar-item"><router-link :to="'/permissions'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Dozvole </span></router-link></li>
+              <li v-if="can('role.view')" class="sidebar-item"><router-link :to="'/roles'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Uloge </span></router-link></li>
             </ul>
           </li>
 
@@ -28,25 +28,16 @@
               <i class="mdi mdi-account-key"></i>
               <span class="hide-menu">Prodavnica </span>
             </a>
-            <ul aria-expanded="false" class="collapse  first-level">
-              <li class="sidebar-item"><router-link :to="'/products'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Proizvodi </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/orders'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Narudžbine </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/customers'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Kupci </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/coupons'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Kuponi </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/brands'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Brendovi </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/categories'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Kategorije </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/properties'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Osobine </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/attributes'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Atributi </span></router-link></li>
-              <li class="sidebar-item"><router-link :to="'/tags'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Tagovi </span></router-link></li>
-            </ul>
-          </li>
-
-          <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-alert"></i><span class="hide-menu">Errors </span></a>
-            <ul aria-expanded="false" class="collapse  first-level">
-              <li class="sidebar-item"><a href="error-403.html" class="sidebar-link"><i class="mdi mdi-alert-octagon"></i><span class="hide-menu"> Error 403 </span></a></li>
-              <li class="sidebar-item"><a href="error-404.html" class="sidebar-link"><i class="mdi mdi-alert-octagon"></i><span class="hide-menu"> Error 404 </span></a></li>
-              <li class="sidebar-item"><a href="error-405.html" class="sidebar-link"><i class="mdi mdi-alert-octagon"></i><span class="hide-menu"> Error 405 </span></a></li>
-              <li class="sidebar-item"><a href="error-500.html" class="sidebar-link"><i class="mdi mdi-alert-octagon"></i><span class="hide-menu"> Error 500 </span></a></li>
+            <ul v-if="user" aria-expanded="false" class="collapse  first-level">
+              <li v-if="can('product.view')" class="sidebar-item"><router-link :to="'/products'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Proizvodi </span></router-link></li>
+              <li v-if="can('order.view')" class="sidebar-item"><router-link :to="'/orders'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Narudžbine </span></router-link></li>
+              <li v-if="can('customer.view')" class="sidebar-item"><router-link :to="'/customers'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Kupci </span></router-link></li>
+              <li v-if="can('coupon.view')" class="sidebar-item"><router-link :to="'/coupons'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Kuponi </span></router-link></li>
+              <li v-if="can('brand.view')" class="sidebar-item"><router-link :to="'/brands'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Brendovi </span></router-link></li>
+              <li v-if="can('category.view')" class="sidebar-item"><router-link :to="'/categories'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Kategorije </span></router-link></li>
+              <li v-if="can('property.view')" class="sidebar-item"><router-link :to="'/properties'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Osobine </span></router-link></li>
+              <li v-if="can('attribute.view')" class="sidebar-item"><router-link :to="'/attributes'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Atributi </span></router-link></li>
+              <li v-if="can('tag.view')" class="sidebar-item"><router-link :to="'/tags'" href="#" class="sidebar-link"><i class="mdi mdi-account-key"></i><span class="hide-menu"> Tagovi </span></router-link></li>
             </ul>
           </li>
         </ul>
@@ -68,7 +59,3 @@
     }
   }
 </script>
-
-<style scoped>
-
-</style>
